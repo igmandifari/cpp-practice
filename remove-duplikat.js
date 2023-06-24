c58a6f23b32555196766a3d0012b63076be50943
Igman Difari <difariivman@gmail.com>
2023-06-24 23:59:35 +0700

    Update remove-duplikat.js

--- remove-duplikat.js
+++ remove-duplikat.js
@@ -1,10 +1,14 @@
 remove duplicates
-let i = 0;
-        for(let j=1; j<nums.length; j++){
-            if(nums[i] !== nums[j]){
-                i++
-                nums[i] = nums[j]
-            }
+let nums = [0,0,1,1,1,2,2,3,3,4]
+
+var rDuplicate = function(nums){
+    let i = 0;
+    for(let j=1; j<nums.length; j++){
+        if(nums[i]!==nums[j]){
+            i++
+            nums[i] = nums[j]
         }
-        return i+1
-};
+    } 
+    return i+1
+} 
+console.log(rDuplicate(nums))
