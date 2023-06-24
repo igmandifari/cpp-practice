0f553bc81d5a9134c75fc7e0d414bcacdf131471
Igman Difari <difariivman@gmail.com>
2023-06-24 17:12:30 +0700

    Create triangle.js

---
+++ triangle.-complete.js
@@ -0,0 +1,24 @@
+let n = 5; // height of pattern
+let string = "";
+// External looap
+for (let i = 1; i <= n; i++) {
+  // Internal loop
+  for (let j = 1; j <= n - i + 1; j++) {
+      if(j%2==0){
+          string += "_"
+      } else{
+          string += n - j + 1
+      }
+  }
+  console.log(string);
+  string = "";
+}
+for (let i = 1; i <= n; i++) {
+  // Internal loop
+  for (let j = 1; j <= i; j++) {
+    string += j
+  }
+  console.log(string);
+  string = "";
+}
+
